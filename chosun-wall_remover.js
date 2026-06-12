270f6513c0b3ef6c54a27b201f25373b6a84fc5b
Anonymous7 <41583431+himawarihome@users.noreply.github.com>
2026-06-12 17:25:31 +0900

    Bump version to 0.3.8 and change run-at timing

--- chosun-wall_remover.js
+++ chosun-wall_remover.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Chosun Wall Remover
 // @description  Chosun Wall Remover
-// @version      0.3.7
+// @version      0.3.8
 // @namespace    http://tampermonkey.net/
 // @author       J W
 // @match        *://*.chosun.com/*
@@ -9,7 +9,7 @@
 // @downloadURL  https://raw.githubusercontent.com/himawarihome/myList/refs/heads/main/chosun-wall_remover.js
 // @updateURL    https://raw.githubusercontent.com/himawarihome/myList/refs/heads/main/chosun-wall_remover.js
 // @grant        unsafeWindow
-// @run-at       document-end
+// @run-at       document-idle
 // ==/UserScript==
 
 (function () {
